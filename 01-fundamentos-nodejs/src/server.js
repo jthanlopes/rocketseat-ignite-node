edac3f507ec8ca95658e675e5c718d4e3b4fc6d8
Jonathan dos Santos <jthan.lopes@gmail.com>
2024-08-08 17:33:26 -0300

    03 - rotas de criação e listagem

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -2,7 +2,17 @@ import http from 'node:http';
 
 // Criar um usuário (name, email, senha)
 const server = http.createServer((request, response) => {
-  return response.end("Hello, world!");
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/users") {
+    return response.end("Listagem de usuários!");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return response.end("Criação de usuário!");
+  }
+
+  return response.end("");
 });
 
 server.listen(3333);
